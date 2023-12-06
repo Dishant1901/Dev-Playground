c9e6742cad45b2e761173d9a7445cbbc24f537c2
Dishant1901 <dishantsinghom@gmail.com>
2023-12-07 01:38:37 +0530

    Initial template

---
+++ play-ground/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div>
+        <h1 className='text-3xl font-bold text-blue-700 px-8 py-6'>
+          Hi this is my react play gorund where i'll getting hands on practice on stuff i learn about react & practice em out!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
