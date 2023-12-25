b1bfd6e897c6cbaddd5cf87e2dd772213e4e1ac8
Dishant1901 <dishantsinghom@gmail.com>
2023-12-25 17:47:30 +0530

    sending body data in GET request

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dev-Playground
-This is a development playground where i'll be practicing various features &amp; learning of BE &amp; FE 
+## This is a development playground where i'll be practicing various features &amp; learning of BE &amp; FE.
+
+## Inlcuding Devops : AWS, CI/CD, Kubernetes ,Docker, Nginx, Jenkins
--- server-playground/index.js
+++ server-playground/index.js
@@ -4,8 +4,12 @@ const app = express();
 
 const PORT = 1001
 
-app.get('/', (req, res) => {
-    res.status(200).json({sucess:true,message:"server running "})
+app.use(express.json())
+
+app.get('/api', (req, res) =>  {
+    const {name,age} = req.body;
+    console.log(name,age)
+    res.status(200).json({sucess:true,message:"server running ",data: {name:name,age:age}})
 });
 
 app.listen(PORT,()=>{
