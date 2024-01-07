dcdd667683c69c8e07138134bfff768a10d1fd2e
Dishant1901 <dishantsinghom@gmail.com>
2024-01-07 08:03:03 +0530

    Verify middleware

---
+++ server-playground/middlewares/verify.js
@@ -0,0 +1,15 @@
+import {User} from '../models/userModel.js'
+import jwt from "jsonwebtoken"
+
+export const verify = async(req,res,next)=>{
+    const token  = req.headers.cookie.split('=')[1]
+    const decodedToken = jwt.verify(token,process.env.ACCESS_SECRET)
+    console.log(decodedToken)
+
+    const user = await User.findById(decodedToken.id)
+    if(!user) {
+        return res.status(401).json(`invalid access token`)
+    }
+    req.user = user;
+    next()
+}
